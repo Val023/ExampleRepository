15006bb18e83d1d0060b601c0248f32cc33aecd1
Valentina <vdronova938@gmail.com>
2023-09-22 14:51:32 +0300

    Example005 IF_Else

---
+++ Example005IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "родион")
+{
+    Console.WriteLine("Хорошего дня, любимый!");
+}
+else
+{
+    Console.Write("Хорошего дня, ");
+    Console.WriteLine(username);
+}
